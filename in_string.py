4d5a116b28c38a4abd6681c693d3c604cb7e21eb
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-05 12:40:07 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,10 +3,12 @@ def check_vowels():
     txt = input("")
     
     print ("Contiene a: " + str("a" in txt.lower()))
+    
     print ("Contiene e: " + str("e" in txt.lower()))
+    
     print ("Contiene i: " + str("i" in txt,lower()))
+    
     print ("Contiene o: " + str("o" in txt.lower()))
+    
     print ("Contiene u: " + str("u" in txt.lower()))
     
-    
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
