4ac495610eea6b89b0b63e465443521a7a38e80c
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-05 12:33:26 -0300

    Tp3 "Uso del in" entregado

--- in_string.py
+++ in_string.py
@@ -2,11 +2,11 @@ def check_vowels():
 
     txt = input("")
     
-    print ("Contiene a: " + str("a" in txt))
-    print ("Contiene e: " + str("e" in txt))
-    print ("Contiene i: " + str("i" in txt))
-    print ("Contiene o: " + str("o" in txt))
-    print ("Contiene u: " + str("u" in txt))
+    print ("Contiene a: " + str("a" in txt.lower()))
+    print ("Contiene e: " + str("e" in txt.lower()))
+    print ("Contiene i: " + str("i" in txt,lower()))
+    print ("Contiene o: " + str("o" in txt.lower()))
+    print ("Contiene u: " + str("u" in txt.lower()))
     
     
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
