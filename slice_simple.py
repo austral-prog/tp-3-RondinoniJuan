b3b8259f7729a54448d18af4210e7cad0559f623
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-05 12:38:02 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,8 +2,8 @@ def slice_simple():
     
     texto = "Awesome"
 
-    print (texto.lower[0:3])
-    print (texto.lower[2:5])
-    print (texto.lower[0:7])
+    print (texto.lower()[0:3])
+    print (texto.lower()[2:5])
+    print (texto.lower()[0:7])
     
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
