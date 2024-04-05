ca69b5f91969bb7e0d7e07eacdcad29aaddfd732
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-05 12:10:44 -0300

    Tp3 "Uso del "slice" con múltiples caracteres" entregado

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,10 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    
+    texto = "Awesome"
 
-# Para verificar este ejercicio ejecutar el comando
+    print (texto.lower[0:3])
+    print (texto.lower[2:5])
+    print (texto.lower[0:7])
+
+    
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
