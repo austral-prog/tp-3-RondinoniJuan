471560b1d4c6c76c3a030c85e3a46bf09523932a
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-04-05 12:39:12 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,12 +3,5 @@ def slice_advanced():
     txt = input("")
     Hello World!
 
-    print (txt[4: :2])
-
-    txt = input("")
-    12345678910
-
-    print (txt[4: :2])
     
-    
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+
